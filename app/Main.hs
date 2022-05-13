d5ec88f53510ae152805882654cc5fbae8621b63
Diogo Castro <dc@diogocastro.com>
2022-05-13 15:17:38 +0100

    Establish WS connection

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import Lib
+import Data.Text qualified as T
+import System.Environment (getEnv)
+import TzBot.Slack.SocketMode.Client as WS
+import TzBot.Slack.WebAPI.Impl (AppLevelToken(..), WebAPIConfig(..))
 
+{- |
+
+Usage:
+
+>  export SLACK_APP_TOKEN='xapp-***'
+>  stack run
+
+You can find your app-level token by going to your Slack App settings and:
+  Basic Information > App-Level Tokens
+
+-}
 main :: IO ()
-main = startApp
+main = do
+  appLevelToken <- getEnv "SLACK_APP_TOKEN"
+  let webApiConfig = WebAPIConfig
+        { wacAppLevelToken = AppLevelToken $ T.pack appLevelToken
+        }
+  WS.wsMain webApiConfig
