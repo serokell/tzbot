79b84b23c1bcd4f4b36b96e5209440d5a2927e9d
Diogo Castro <dc@diogocastro.com>
2022-05-13 16:41:20 +0100

    Remove unnecessary template modules

---
+++ test/Main.hs
@@ -0,0 +1,13 @@
+-- SPDX-FileCopyrightText: 2022 Serokell <https://serokell.io>
+--
+-- SPDX-License-Identifier: MPL-2.0
+
+module Main
+  ( main
+  ) where
+
+import Test.Tasty
+import Tree (tests)
+
+main :: IO ()
+main = tests >>= defaultMain
