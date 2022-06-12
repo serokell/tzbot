f2fb4424bce4a925cadd187330398f9239e6555c
Ilona Prikule <ilona.prikule@serokell.io>
2022-06-12 17:12:04 +0200

    [#3] Augment websockets loop
    Problem: our websockets app doesn't currently handle the incoming
    Slack events in any useful way. Additionally, we are trying to
    implement the AI ourselves, whereas there is an external library
    already doing that.
    
    Solution: changed the websockets loop to use the 'slacker' lib,
    added extracting useful data from any message events, as well as
    some safeguards.

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,20 @@
 
 module Main where
 
+import Control.Monad (void)
+import Data.Function ((&))
 import Data.Text qualified as T
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import System.Environment (getEnv)
-import TzBot.Slack.SocketMode.Client as WS
-import TzBot.Slack.WebAPI.Impl (AppLevelToken(..), BotToken(..), WebAPIConfig(..))
+
+import Slacker
+  (SlackConfig, SocketModeEvent(..), defaultSlackConfig, handleThreadExceptionSensibly,
+  pattern Event, runSocketMode, setApiToken, setAppToken, setOnException)
+
+import TzBot.ProcessEvent (constructEventSummary, processEvent)
+import TzBot.Slack.WebAPI.Impl
+  (AppLevelToken(..), BotToken(..), WebAPIConfig(..), WebAPIState(..), runWebAPIM)
 
 {- |
 
@@ -22,10 +32,24 @@ To generate app-level / bot tokens, see: <docs/development.md>
 -}
 main :: IO ()
 main = do
-  appLevelToken <- getEnv "SLACK_TZ_APP_TOKEN"
-  botToken <- getEnv "SLACK_TZ_BOT_TOKEN"
-  let webApiConfig = WebAPIConfig
-        { wacAppLevelToken = AppLevelToken $ T.pack appLevelToken
-        , wacBotToken = BotToken $ T.pack botToken
-        }
-  WS.wsMain webApiConfig
+  appLevelToken <- T.pack <$> getEnv "SLACK_TZ_APP_TOKEN"
+  botToken <- T.pack <$> getEnv "SLACK_TZ_BOT_TOKEN"
+  let sCfg = defaultSlackConfig
+            & setApiToken botToken
+            & setAppToken appLevelToken
+            & setOnException handleThreadExceptionSensibly -- auto-handle disconnects
+
+  let wCfg = WebAPIConfig {
+    wacAppLevelToken = AppLevelToken appLevelToken
+  , wacBotToken = BotToken botToken
+  }
+
+  manager <- newManager tlsManagerSettings
+  let wState = WebAPIState wCfg manager
+
+  runSocketMode sCfg (handler wState) -- auto-acknowledge received messages
+
+handler :: WebAPIState -> SlackConfig -> SocketModeEvent -> IO ()
+handler wstate _cfg = \case
+  Event "message" evt -> void . runWebAPIM wstate $ processEvent (constructEventSummary evt)
+  _ -> pure ()
