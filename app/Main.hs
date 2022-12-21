ef37a82923f675d4d96c8a6e4c84ab286b5e6784
Yuri Romanowski <yuri.romanowski@serokell.io>
2022-12-21 20:23:18 +0500

    Move bot starting code to the library
    It's better to have minimum of code in the `app` directory because
    there will not be many executables and this way handling dependencies
    is easier.

--- app/Main.hs
+++ app/Main.hs
@@ -4,52 +4,7 @@
 
 module Main where
 
-import Control.Monad (void)
-import Data.Function ((&))
-import Data.Text qualified as T
-import Network.HTTP.Client (newManager)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
-import System.Environment (getEnv)
+import TzBot.BotMain qualified as BotMain
 
-import Slacker
-  (SlackConfig, SocketModeEvent(..), defaultSlackConfig, handleThreadExceptionSensibly,
-  pattern EventValue, runSocketMode, setApiToken, setAppToken, setOnException)
-
-import TzBot.ProcessEvent (constructEventSummary, processEvent)
-import TzBot.Slack.WebAPI.Impl
-  (AppLevelToken(..), BotToken(..), WebAPIConfig(..), WebAPIState(..), runWebAPIM)
-
-{- |
-
-Usage:
-
->  export SLACK_TZ_APP_TOKEN='xapp-***'
->  export SLACK_TZ_BOT_TOKEN='xoxb-***'
->  stack run
-
-To generate app-level / bot tokens, see: <docs/development.md>
-
--}
 main :: IO ()
-main = do
-  appLevelToken <- T.pack <$> getEnv "SLACK_TZ_APP_TOKEN"
-  botToken <- T.pack <$> getEnv "SLACK_TZ_BOT_TOKEN"
-  let sCfg = defaultSlackConfig
-            & setApiToken botToken
-            & setAppToken appLevelToken
-            & setOnException handleThreadExceptionSensibly -- auto-handle disconnects
-
-  let wCfg = WebAPIConfig {
-    wacAppLevelToken = AppLevelToken appLevelToken
-  , wacBotToken = BotToken botToken
-  }
-
-  manager <- newManager tlsManagerSettings
-  let wState = WebAPIState wCfg manager
-
-  runSocketMode sCfg (handler wState) -- auto-acknowledge received messages
-
-handler :: WebAPIState -> SlackConfig -> SocketModeEvent -> IO ()
-handler wstate _cfg = \case
-  EventValue "message" evt -> void . runWebAPIM wstate $ processEvent (constructEventSummary evt)
-  _ -> pure ()
+main = BotMain.main
